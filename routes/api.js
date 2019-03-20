6958e60889d0799e3c5792ab4cc150954a5dff07
Christian Terence <chrisc98@vt.edu>
2019-03-20 16:10:07 -0400

    fixed a bug

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 module.exports = app => {
   app.get("/blabs", (req, res) => {
-    const params = req.params;
+    const params = req.query;
     const timestamp = params.createdSince;
 
     //parse the mongo db to get all time stamp
@@ -19,18 +19,17 @@ module.exports = app => {
 
   app.delete("/blabs/:id", (req, res) => {
       const id = req.params.id;
-      
-
+    
       //if id is in mongodb 
       res.status(200).send("Blab deleted successfully");
 
 
       //if id is not
-      res.status(404).send("HELLO");
+      // res.status(404).send("HELLO");
   });
 
   app.post("/blabs", (req, res) => {
-    const params = req.params;
+    const params = req.query
     const author = params.author;
     const message = params.message;
 
