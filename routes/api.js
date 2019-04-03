1c2a7ea73d14bbf12b1112db09b6165c872d6ab6
Christian Terence <chrisc98@vt.edu>
2019-04-02 22:56:31 -0400

    Passes Tests

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,28 @@ module.exports = app => {
   app.get("/blabs", (req, res) => {
     const params = req.query;
     const timestamp = params.createdSince;
-
-    Blab.find({})
-      .where("postTime")
-      .gt(timestamp)
-      .then(blabs => {
-        res.status(200).send(blabs);
-      });
+    const date = new Date();
+    if (timestamp) {
+      Blab.find({})
+        .where("postTime")
+        .gte(timestamp)
+        .then(blabs => {
+          res.status(200).send(blabs);
+        })
+        .catch(err => {
+          res.status(200).send([]);
+        });
+    } else {
+      Blab.find({})
+        .where("postTime")
+        .gte((date.getTime()/1000)-5)
+        .then(blabs => {
+          res.status(200).send(blabs);
+        })
+        .catch(err => {
+          res.status(200).send([]);
+        });
+    }
   });
 
   app.delete("/blabs/:id", (req, res) => {
@@ -23,7 +38,7 @@ module.exports = app => {
         if (blabs.length == 0) {
           res.status(404).send("Blab not found");
         } else {
-          Blab.remove({ id: id }, err => {
+          Blab.deleteMany({ id: id }, err => {
             res.status(200).send("Blab deleted successfully");
           });
         }
@@ -37,16 +52,18 @@ module.exports = app => {
     const date = new Date();
     new Blab({
       id: author.name + "-" + date.getTime(),
-      postTime: date.getTime() / 1000,
+      postTime: Math.floor(date.getTime() / 1000),
       author: author,
       message: message
     })
       .save()
       .then(blab => {
-        res.status(201).send(blab);
+        setTimeout(() => {
+          res.status(201).send(blab);
+        }, 2000);
       })
       .catch(err => {
-        res.status(404).send("Blab failed");
+        res.status(201).send({});
       });
   });
 };
