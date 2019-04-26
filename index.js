55b13f4e9459ee520eb51430f1c68dab427f2a3d
Christian Terence <chrisc98@vt.edu>
2019-04-26 11:18:19 -0400

    User and Pass Req?

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+var fs = require("fs");
+
 require("./models/blabs");
 
-mongoose.connect("mongodb://mongo:27017", (err) => {
+var mongoUser = "user";
+var mongoPass = fs
+  .readFileSync("./db_password.txt")
+  .toString()
+  .split("\n")[0];
+
+mongoose.connect(`mongodb://${encodeURIComponent(mongoUser)}:${encodeURIComponent(mongoPass)}@mongo:27017`, err => {
   if (err) {
-      console.log("DB failed");
-    } else {
-      console.log("connected");
-    }
+    console.log(err);
+    console.log("DB failed");
+  } else {
+    console.log("connected");
+  }
 });
 
 app.use(bodyParser.json());
